041b8ca380a7d59a5960579a69bc8393ae1b5d6b
Danny McVey <dannybmcvey@gmail.com>
2023-07-26 07:50:28 -0700

    hfra data quality tool

---
+++ hrfa_match.py
@@ -0,0 +1,46 @@
+import arcpy
+
+# Function to check if HFTDClass field exists in the input layer
+def field_exists(layer, field_name):
+    field_list = [field.name for field in arcpy.ListFields(layer)]
+    return field_name in field_list
+
+def main():
+    # Get the parameters from the geoprocessing tool
+    lyr = arcpy.GetParameterAsText(0)  # Input Layer
+
+    # Check if the input layer has the required field 'HFTDClass'
+    if not field_exists(lyr, "HFTDClass"):
+        arcpy.AddError("The field 'HFTDClass' does not exist in the input layer.")
+        return
+
+    # Run Spatial Join Geoprocessing Tool
+    arcpy.analysis.SpatialJoin(
+        target_features=lyr,
+        join_features=r"https://services5.arcgis.com/z6hI6KRjKHvhNO0r/arcgis/rest/services/SCE_HighFireRiskArea/FeatureServer\0",
+        out_feature_class=f"{lyr}_SpatialJoin",
+    )
+
+    arcpy.AddMessage(f"Created {lyr}_SpatialJoin")
+
+    arcpy.AddMessage(f"Select By Attributes")
+    arcpy.management.SelectLayerByAttribute(
+        in_layer_or_view=f"{lyr}_SpatialJoin",
+        selection_type="NEW_SELECTION",
+        where_clause="HFTDClass <> 'Non-HFTD' AND (LABEL IS NULL OR LABEL = '')",
+    )
+
+    # Get the count of selected features
+    count = int(arcpy.GetCount_management(f"{lyr}_SpatialJoin")[0])
+    arcpy.AddMessage(f"{lyr}_SpatialJoin feature count: {count}")
+
+    # Check if there are problematic situations, and indicate failure with AddError
+    if count > 0:
+        arcpy.AddError(f"There are {count} assets located outside the specified HFTD Boundary.")
+        return
+
+    # Clear the selection
+    arcpy.management.SelectLayerByAttribute(f"{lyr}_SpatialJoin", "CLEAR_SELECTION")
+
+if __name__ == "__main__":
+    main()
