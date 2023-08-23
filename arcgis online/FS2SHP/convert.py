2577f5072d5431354d54c380aec1d79068c58523
Danny McVey <dannybmcvey@gmail.com>
2023-08-23 15:11:27 -0700

    refactor for desired folder structure

--- arcgis online/FS2SHP/convert.py
+++ arcgis online/FS2SHP/convert.py
@@ -1,3 +1,17 @@
+'''
+desired folder structure:
+WestCovina_SLs
+    WorkOrders_DateTime.zip
+        WorkOrders_DateTime.zip
+            WorkOrders_DateTime.shp
+        separate
+            WorkOrder1_DateTime.zip
+                WorkOrder1_DateTime.shp
+            WorkOrder2_DateTime.zip
+                WorkOrder2_DateTime.shp
+                    
+'''
+
 import arcpy
 from arcgis.gis import GIS
 import datetime
@@ -5,10 +19,10 @@ import zipfile
 import os
 import shutil
 
-def explode_shapefile(input_shp, output_folder):
+def explode_shapefile(input_shp, output_folder, today_date):
     """Explodes a shapefile with polygons into separate shapefiles based on 'WorkOrder' attribute"""
 
-    # todo: replace with Split By Attributes (Analysis)
+    # consider replacing some of the logic below with Split By Attributes (Analysis)
     # arcpy.analysis.SplitByAttributes(Input_Table, Target_Workspace, Split_Fields)
     # https://pro.arcgis.com/en/pro-app/latest/tool-reference/analysis/split-by-attributes.htm
     
@@ -22,19 +36,35 @@ def explode_shapefile(input_shp, output_folder):
     # Loop through features
     with arcpy.da.SearchCursor(input_shp, ["WorkOrder","SHAPE@","FID"]) as cursor:
         for row in cursor:
-            work_order = row[0]
-            polygon = row[1]
+            work_order = row[0]            
+            out_name = f"{shp_name}_{work_order}"
             
             # Check if work order is populated
             if work_order:
-                out_shp = os.path.join(output_folder, f"{shp_name}_{work_order}.shp")
+                out_shp = os.path.join(output_folder, f"{out_name}.shp")
                 
                 # Export single feature to new shapefile
-                if not os.path.exists(out_shp):
+                # print(out_shp)
+                if not os.path.exists(out_shp) and not out_shp.endswith(" .shp"):
+                    # print(os.path.exists(output_folder))
                     arcpy.conversion.ExportFeatures(input_shp, out_shp, f'"FID" = {row[2]}')
+                    
+                    # Zip separate shapefile folder
+                    zip_name = os.path.join(output_folder,f"{out_name}.zip")
+                    all_files = []
+                    for f in os.listdir(os.path.join(output_folder)):
+                        # print(f,work_order)
+                        if os.path.isfile(os.path.join(output_folder, f)) and work_order in f:
+                            all_files.append(f)
+                    # print(all_files)
+                    zip_file = zipfile.ZipFile(zip_name, "w")
+                    for file in all_files:
+                        zip_file.write(os.path.join(output_folder, file), arcname=file)
+                    zip_file.close()
 
-    print("Done exploding shapefile")
-
+                    # Delete shapefile and folder
+                    arcpy.Delete_management(os.path.join(output_folder,out_shp))
+                    arcpy.Delete_management(os.path.join(output_folder, f"{shp_name}_{work_order}"))
 
 # Authentication 
 gis = GIS("pro")
@@ -70,24 +100,25 @@ if os.path.exists(count_file):
         arcpy.management.CopyFeatures(service_item.url+r"/0", os.path.join(onedrive_folder,out_name,out_name) + ".shp")
 
         # Zip shapefile folder
-        zip_name = os.path.join(onedrive_folder,f"WorkOrders_{today_date}.zip")
-        all_files = [f for f in os.listdir(os.path.join(onedrive_folder,out_name)) if os.path.isfile(os.path.join(onedrive_folder,out_name, f))]
+        zip_name = os.path.join(onedrive_folder,out_name,f"WorkOrders_{today_date}.zip")
+        all_files = [f for f in os.listdir(os.path.join(onedrive_folder,out_name))
+                     if os.path.isfile(os.path.join(onedrive_folder,out_name, f))]
         zip_file = zipfile.ZipFile(zip_name, "w")
         for file in all_files:
             zip_file.write(os.path.join(onedrive_folder,out_name, file), arcname=file)
         zip_file.close()
 
-        # Set folder name
-        folder_name = "Shapefile exports"
+        # # Set ArcGIS online folder name
+        # folder_name = "Shapefile exports"
 
-        # Publish shapefile to folder
-        new_item = gis.content.add({"type":"Shapefile","title": f"WorkOrders_{today_date}"}, 
-                                   os.path.join(onedrive_folder,f"WorkOrders_{today_date}.zip"),folder = folder_name)
+        # # Publish shapefile item to folder
+        # new_item = gis.content.add({"type":"Shapefile","title": f"WorkOrders_{today_date}"}, 
+        #                            os.path.join(onedrive_folder,f"WorkOrders_{today_date}.zip"),folder = folder_name)
 
-        # Set sharing level                         
-        new_item.share(org=True)
+        # # Set sharing level                         
+        # new_item.share(org=True)
 
-        print(new_item.homepage)
+        # print("published:",new_item.homepage)
         
         # Overwrite count file
         with open(count_file,"w") as f:
@@ -95,21 +126,23 @@ if os.path.exists(count_file):
 
         # Explode shapefile into separate work order shapefiles
         input_shp = os.path.join(onedrive_folder,out_name,out_name) + ".shp"
-        explode_shapefile(input_shp,os.path.join(os.path.dirname(input_shp),"exploded"))
+        explode_shapefile(input_shp,os.path.join(os.path.dirname(input_shp),"separate"),today_date)
+
+        arcpy.Delete_management(os.path.join(input_shp))
 
         # Zip the entire output directory
-        zip_name = os.path.join(onedrive_folder, f"WorkOrders_{today_date}_separate") 
+        zip_name = os.path.join(onedrive_folder, f"WorkOrders_{today_date}") 
         out_dir = os.path.join(onedrive_folder,out_name)
         shutil.make_archive(zip_name,'zip',out_dir)
         # print(out_dir)
 
-        os.remove(out_dir)
+        arcpy.Delete_management(os.path.join(out_dir))
 
     else:
         print("No new features. Skipping export.")
 else:        
     # Write initial count
-    with open(os.path.join(onedrive_folder,"count.txt"),"w") as f:
+    with open(os.path.join(onedrive_folder+"_count","count.txt"),"w") as f:
         f.write(str(count))
 
 print("Done")
