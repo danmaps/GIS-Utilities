5b607947ffabd59b7bdc8edb0c4b9b85de5e0024
Danny McVey <dannybmcvey@gmail.com>
2023-10-19 17:36:37 -0700

    fix bug with counting uniques

--- arcgis pro/fill_selected.py
+++ arcgis pro/fill_selected.py
@@ -1,22 +1,31 @@
 def check_data(layer, fields):
-    # Iterate over the specified fields
     for field in fields:
         print(f"looking at field: {field}")
-        # Count unique non-null values
-        unique_values = {row[0] for row in cursor if row[0] is not None}
-        print(f"unique values: {unique_values}")
-        # Check if there is only one distinct non-null value
-        if len(unique_values) > 1:
-            print(field,f"more than one nonnull value {unique_values}")
-            return # don't do the fill
-        # Fill in values
-        query = f"{field} IS NULL OR {field} = ''"
-        with arcpy.da.UpdateCursor(layer, [field], query) as cursor:
-            # Update null values with the non-null value in all null rows
+        
+        # Get a set of unique non-null values for the current field
+        unique_values = set()
+        with arcpy.da.SearchCursor(layer, field) as cursor:
             for row in cursor:
-                row[0] = list(unique_values)[0]
-                cursor.updateRow(row)
+                if row[0] is not None:
+                    unique_values.add(row[0])
+        
+        print(f"unique values: {unique_values}")
+
+        if len(unique_values) > 1:
+            print(field, f"more than one nonnull value {unique_values}")
+        else:
+            # Fill in values
+            query = f"{field} IS NULL OR {field} = ''"
+            with arcpy.da.UpdateCursor(layer, field, query) as update_cursor:
+                for row in update_cursor:
+                    row[0] = list(unique_values)[0]
+                    update_cursor.updateRow(row)
+    
     return True
+
+
+    return True
+
 
 layer = "Street Lights"
 fields = ["SUB_FLOC", "SUB"]
