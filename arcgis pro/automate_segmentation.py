5ec3a2eca029bb5fb4f18d1e6ab3d79025c8f364
Danny McVey <dannybmcvey@gmail.com>
2023-12-29 13:11:14 -0800

    refactor

--- arcgis pro/automate_segmentation.py
+++ arcgis pro/automate_segmentation.py
@@ -5,136 +5,147 @@ from arcgis.features import FeatureLayer
 
 # Log into ArcGIS Online
 gis = GIS("Pro")
-
 def script_tool(param0, param1):
-
-    def check_missing_fields(layer, field_mapping):
-        """
-        Checks for missing fields in the specified layer based on the provided field mapping.
-        If missing fields are found, an ArcPy warning is displayed.
-
-        Parameters:
-        layer (str): The name or path of the layer to check.
-        field_mapping (str): The field mapping string.
-
-        Returns:
-        None
-        """
-        # List all fields in the layer
-        fields = [field.name for field in arcpy.ListFields(layer)]
-
-        # Parse field mapping to get field names
-        mapped_fields = [f.split(' ')[0] for f in field_mapping.split(';')]
-
-        # Check if each field in the field mapping exists in the layer
-        missing_fields = [f for f in mapped_fields if f not in fields]
-
-        # Add warning if missing fields are found
-        if missing_fields:
-            warning_message = "Missing fields in the layer '{}': {}".format(layer, ', '.join(missing_fields))
-            arcpy.AddWarning(warning_message)
-
-    
     arcpy.AddMessage("MakeFeatureLayer...")
+
     arcpy.MakeFeatureLayer_management(gis.content.get("80c86dd5aa184264896437b0e1d8e54b").layers[0].url, "bulktrans")
     arcpy.MakeFeatureLayer_management(gis.content.get("b653952755af48fd85a31680e07a6641").layers[0].url, "subtrans")
+
     arcpy.MakeFeatureLayer_management(gis.content.get("38a2e08043e04268a1a386e63c3c69bd").layers[0].url, "sce_HighFireRiskArea")
     arcpy.MakeFeatureLayer_management(gis.content.get("71e126217f44483091982e52804e4171").layers[1].url, "Subtrans_Segments")
-
-    arcpy.AddMessage("ExportFeatures...")
-    bulktrans_field_mapping='CIRCUIT_ID "CIRCUIT_ID" true true false 50 Text 0 0,First,#,bulktrans,CIRCUIT_ID,0,50;CIRCUIT_NAME "CIRCUIT_NAME" true true false 150 Text 0 0,First,#,bulktrans,CIRCUIT_NAME,0,150;KV "KV" true true false 0 Double 0 0,First,#,bulktrans,KV,-1,-1'
-    check_missing_fields("bulktrans",bulktrans_field_mapping)
-    arcpy.conversion.ExportFeatures(
-        in_features="bulktrans",
-        out_features="bulktrans_ExportFeatures",
-        where_clause="",
-        use_field_alias_as_name="NOT_USE_ALIAS",
-        field_mapping=bulktrans_field_mapping,
-        sort_field=None,
-    )
-
-    subtrans_field_mapping = 'CIRCUIT_NO "CIRCUIT_NO" true true false 7 Text 0 0,First,#,subtrans,CIRCUIT_NO,0,7;NAME "NAME" true true false 70 Text 0 0,First,#,subtrans,NAME,0,70;KV "KV" true true false 0 Double 0 0,First,#,subtrans,KV,-1,-1'
-    check_missing_fields("subtrans",subtrans_field_mapping)
-    arcpy.conversion.ExportFeatures(
-        in_features="subtrans",
-        out_features="subtrans_ExportFeatures",
-        where_clause="",
-        use_field_alias_as_name="NOT_USE_ALIAS",
-        field_mapping=subtrans_field_mapping,
-        sort_field=None,
-    )
-
-    arcpy.AddMessage("Clip and intersect...")
-
-    # clip to HFRA and intersect with Segments
-    def clip_intersect(lyr):
-        arcpy.analysis.Clip(
-            in_features=lyr,
-            clip_features="sce_HighFireRiskArea",
-            out_feature_class=rf"{lyr}_Clip",
-        )
-        arcpy.analysis.Intersect(
-            in_features=rf"{lyr}_Clip #;Subtrans_Segments #",
-            out_feature_class=rf"{lyr}_Intersect",
-        )
-
-    clip_intersect("bulktrans_ExportFeatures")
-    clip_intersect("subtrans_ExportFeatures")
-
-    arcpy.AddMessage("Spatial Joins...")
 
     arcpy.MakeFeatureLayer_management(gis.content.get("4414e648847445ffa6019523850f8d7f").layers[0].url, "districts")
     arcpy.MakeFeatureLayer_management(gis.content.get("9998b507eef44f9db5ff962268d08ab6").layers[0].url, "counties")
 
+    # clip to HFRA and intersect with Segments
+    arcpy.AddMessage("clip to HFRA and intersect with Segments...")
+
+    def clip_intersect(lyr):
+        arcpy.analysis.Clip(
+            in_features=lyr,
+            clip_features="sce_HighFireRiskArea",
+            out_feature_class=f"{lyr}_Clip",
+        )
+        arcpy.analysis.Intersect(
+            in_features=f"{lyr}_Clip #;Subtrans_Segments #",
+            out_feature_class=rf"{lyr}_Intersect",
+        )
+    clip_intersect("bulktrans")
+    clip_intersect("subtrans")
+
+    def create_field_mappings(target, join, join_field, output_field_name=None, merge_rule=None, delimiter=', ', output_field_length=255):
+        """
+        Creates field mappings for spatial join, setting output field as text type and specified length.
+
+        Parameters:
+        target (str): The target feature class for the join.
+        join (str): The join feature class.
+        join_field (str): The field name in the join feature class to match properties.
+        output_field_name (str, optional): The name for the output field. Default is the same as join_field.
+        merge_rule (str, optional): The merge rule to apply ('Join', 'Sum', 'Mean', etc.). Default is None.
+        delimiter (str, optional): The delimiter for 'Join' merge rule. Default is ', '.
+        output_field_length (int, optional): The length for the output field. Default is 255.
+
+        Returns:
+        arcpy.FieldMappings: The field mappings for the spatial join.
+        """
+        # Create field mappings
+        field_mappings = arcpy.FieldMappings()
+        field_mappings.addTable(target)
+
+        # Create and set up the field map
+        field_map = arcpy.FieldMap()
+        field_map.addInputField(join, join_field)
+
+        # Set output field properties
+        out_field = arcpy.Field()
+        out_field.name = output_field_name if output_field_name else join_field
+        out_field.aliasName = out_field.name
+        if output_field_name == 'COUNTY':
+            out_field.type = "String"  # Explicitly set as text type
+            out_field.length = output_field_length  # Set length to 255
+
+        if merge_rule:
+            field_map.mergeRule = merge_rule
+            if merge_rule.lower() == 'join':
+                field_map.joinDelimiter = delimiter
+
+        field_map.outputField = out_field
+
+        # Add the field map to the field mappings
+        field_mappings.addFieldMap(field_map)
+
+        return field_mappings
+
+    arcpy.AddMessage("SpatialJoin to counties and districts...")
+
     arcpy.analysis.SpatialJoin(
-        target_features="bulktrans_ExportFeatures_Intersect",
+        target_features="bulktrans_Intersect",
         join_features="counties",
-        out_feature_class="bulktrans_join1",
+        out_feature_class="bulktrans_counties",
         join_operation="JOIN_ONE_TO_ONE",
         join_type="KEEP_ALL",
-        field_mapping='CIRCUIT_NO "CIRCUIT_NO" true true false 50 Text 0 0,First,#,bulktrans_ExportFeatures_Intersect,CIRCUIT_NO,0,50;CIRCUIT_NA "CIRCUIT_NA" true true false 150 Text 0 0,First,#,bulktrans_ExportFeatures_Intersect,CIRCUIT_NA,0,150;VOLTAGE "VOLTAGE" true true false 8 Double 0 0,First,#,bulktrans_ExportFeatures_Intersect,VOLTAGE,-1,-1;SEGMENTS "SEGMENTS" true true false 5 Text 0 0,First,#,bulktrans_ExportFeatures_Intersect,SEGMENTS,0,5;COUNTY "COUNTY" true true false 150 Text 0 0,Join,", ",counties,NAME,0,150',
         match_option="INTERSECT",
-        search_radius=None,
-        distance_field_name="",
+        field_mapping=create_field_mappings(
+            target="bulktrans_Intersect", 
+            join="counties", 
+            join_field="NAME",
+            output_field_name="COUNTY",
+            merge_rule="Join"
+        )
     )
 
     arcpy.analysis.SpatialJoin(
-        target_features="bulktrans_join1",
+        target_features="bulktrans_counties",
         join_features="districts",
-        out_feature_class="bulktrans_join2",
+        out_feature_class="bulktrans_districts",
         join_operation="JOIN_ONE_TO_ONE",
         join_type="KEEP_ALL",
-        field_mapping='CIRCUIT_NO "CIRCUIT_NO" true true false 50 Text 0 0,First,#,bulktrans_join1,CIRCUIT_NO,0,50;CIRCUIT_NAME "CIRCUIT_NAME" true true false 150 Text 0 0,First,#,bulktrans_join1,CIRCUIT_NAME,0,150;VOLTAGE "VOLTAGE" true true false 8 Double 0 0,First,#,bulktrans_join1,VOLTAGE,-1,-1;SEGMENTS "SEGMENTS" true true false 5 Text 0 0,First,#,bulktrans_join1,SEGMENTS,0,5;COUNTY "COUNTY" true true false 150 Text 0 0,First,#,bulktrans_join1,COUNTY,0,150;DISTRICT "DISTRICT" true true false 2 Text 0 0,First,#,districts,NUMBER_,0,2',
         match_option="INTERSECT",
-        search_radius=None,
-        distance_field_name="",
+        field_mapping=create_field_mappings(
+            target="bulktrans_counties", 
+            join="districts",
+            join_field="NUMBER_",
+            output_field_name='DISTRICT'
+        )
+    )
+
+    arcpy.management.AlterField(
+        in_table="subtrans_Intersect",
+        field="NAME",
+        new_field_name="CIRCUIT_NAME"
     )
 
     arcpy.analysis.SpatialJoin(
-        target_features="subtrans_ExportFeatures_Intersect",
+        target_features="subtrans_Intersect",
         join_features="counties",
-        out_feature_class="subtrans_join1",
+        out_feature_class="subtrans_counties",
         join_operation="JOIN_ONE_TO_ONE",
         join_type="KEEP_ALL",
-        field_mapping='CIRCUIT_NO "CIRCUIT_NO" true true false 50 Text 0 0,First,#,subtrans_ExportFeatures_Intersect,CIRCUIT_NO,0,50;CIRCUIT_NA "CIRCUIT_NA" true true false 150 Text 0 0,First,#,subtrans_ExportFeatures_Intersect,CIRCUIT_NA,0,150;VOLTAGE "VOLTAGE" true true false 8 Double 0 0,First,#,subtrans_ExportFeatures_Intersect,VOLTAGE,-1,-1;SEGMENTS "SEGMENTS" true true false 5 Text 0 0,First,#,subtrans_ExportFeatures_Intersect,SEGMENTS,0,5;COUNTY "COUNTY" true true false 150 Text 0 0,Join,", ",counties,NAME,0,150',
         match_option="INTERSECT",
-        search_radius=None,
-        distance_field_name="",
+        field_mapping=create_field_mappings(
+            target="subtrans_Intersect", 
+            join="counties", 
+            join_field="NAME",
+            output_field_name="COUNTY",
+            merge_rule="Join"
+        )
     )
 
     arcpy.analysis.SpatialJoin(
-        target_features="subtrans_join1",
+        target_features="subtrans_counties",
         join_features="districts",
-        out_feature_class="subtrans_join2",
+        out_feature_class="subtrans_districts",
         join_operation="JOIN_ONE_TO_ONE",
         join_type="KEEP_ALL",
-        field_mapping='CIRCUIT_NO "CIRCUIT_NO" true true false 50 Text 0 0,First,#,subtrans_join1,CIRCUIT_NO,0,50;CIRCUIT_NA "CIRCUIT_NA" true true false 150 Text 0 0,First,#,subtrans_join1,CIRCUIT_NA,0,150;VOLTAGE "VOLTAGE" true true false 8 Double 0 0,First,#,subtrans_join1,VOLTAGE,-1,-1;SEGMENTS "SEGMENTS" true true false 5 Text 0 0,First,#,subtrans_join1,SEGMENTS,0,5;COUNTY "COUNTY" true true false 150 Text 0 0,First,#,subtrans_join1,COUNTY,0,150;DISTRICT "DISTRICT" true true false 2 Text 0 0,First,#,districts,NUMBER_,0,2',
         match_option="INTERSECT",
-        search_radius=None,
-        distance_field_name="",
+        field_mapping=create_field_mappings(
+            target="subtrans_counties", 
+            join="districts",
+            join_field="NUMBER_",
+            output_field_name='DISTRICT'
+        )
     )
-
-    arcpy.AddMessage("Adding new fields...")
 
     def validate_and_calculate_field(layer, field, expression):
         # Validate field exists
@@ -156,35 +167,64 @@ def script_tool(param0, param1):
         arcpy.management.CalculateField(layer, field, expression, "PYTHON3")
 
     # Add missing fields
-    for lyr in [bulk_layer, sub_layer]:
+    arcpy.AddMessage("Add missing fields...")
+
+    for lyr in ["bulktrans_districts", "subtrans_districts"]:
         arcpy.management.AddFields(
             in_table=lyr,
             field_description="CIRCUIT_SE TEXT # 255 # #;WIRE_CLASS TEXT # 50 # #;SUBSTATION TEXT # 255 # #;GUST_THRES LONG # # # #;WIND_THRES LONG # # # #",
         )
+    arcpy.AddMessage("Calculate fields...")
     # Calculate BulkTrans fields
-    validate_and_calculate_field(bulk_layer, "CIRCUIT_SE", "!CIRCUIT_NA!.upper()+'_'+!SEGMENTS!")
-    validate_and_calculate_field(bulk_layer, "WIRE_CLASS", '"Transmission"')
-    validate_and_calculate_field(bulk_layer, "SUBSTATION", "!CIRCUIT_NA!.split('-')[0].upper()")
+    validate_and_calculate_field("bulktrans_districts", "CIRCUIT_SE", "!CIRCUIT_NAME!.upper()+'_'+!SEGMENTS!")
+    validate_and_calculate_field("bulktrans_districts", "WIRE_CLASS", '"Transmission"')
+    validate_and_calculate_field("bulktrans_districts", "SUBSTATION", "!CIRCUIT_NAME!.split('-')[0].upper()")
 
     # Calculate SubTrans fields
-    validate_and_calculate_field(sub_layer, "CIRCUIT_NO", "'ET-'+!CIRCUIT_NO!.split('-')[0]")  # 00158-BT -> ET-00158
-    validate_and_calculate_field(sub_layer, "CIRCUIT_SE", "!CIRCUIT_NA!.upper()+'_'+!SEGMENTS!")
-    validate_and_calculate_field(sub_layer, "WIRE_CLASS", '"Transmission"')
-    validate_and_calculate_field(sub_layer, "SUBSTATION", "!CIRCUIT_NA!.split('-')[0].upper()")
+    # validate_and_calculate_field("subtrans_districts", "CIRCUIT_NO", "'ET-'+!CIRCUIT_NO!.split('-')[0]")  # 00158-BT -> ET-00158
+    validate_and_calculate_field("subtrans_districts", "CIRCUIT_SE", "!CIRCUIT_NAME!.upper()+'_'+!SEGMENTS!")
+    validate_and_calculate_field("subtrans_districts", "WIRE_CLASS", '"Transmission"')
+    validate_and_calculate_field("subtrans_districts", "SUBSTATION", "!CIRCUIT_NAME!.split('-')[0].upper()")
 
-    # Remove extra fields
-    for lyr in [bulk_layer, sub_layer]:
-        arcpy.management.DeleteField(
-            in_table=lyr, drop_field="Join_Count;TARGET_FID;", method="DELETE_FIELDS"
-        )
+    def schema_conform(source_fc, reference_fc):
+        """
+        Adjusts the schema of the source feature class to match the reference feature class by removing extra fields,
+        excluding system-managed fields.
 
-    arcpy.AddMessage("Exporting...")
+        Parameters:
+        source_fc (str): Path to the source feature class whose schema needs adjustment.
+        reference_fc (str): Path to the reference feature class to match schema with.
+        """
+        # System-managed fields that should not be removed
+        protected_fields = {'OBJECTID', 'Shape_Length', 'Shape_Area', 'FID', 'Shape'}
 
-    arcpy.conversion.ExportFeatures(bulk_layer, param0)
-    arcpy.conversion.ExportFeatures(sub_layer, param1)
+        # Get field names for both feature classes
+        source_fields = {f.name for f in arcpy.ListFields(source_fc) if f.name not in protected_fields}
+        reference_fields = {f.name for f in arcpy.ListFields(reference_fc)}
+
+        # Identify fields to remove (present in source but not in reference)
+        fields_to_remove = source_fields - reference_fields
+
+        # Remove extra fields from source feature class
+        if fields_to_remove:
+            arcpy.DeleteField_management(source_fc, list(fields_to_remove))
+            print(f"Removed fields from {source_fc}: {fields_to_remove}")
+        else:
+            print(f"No fields to remove. {source_fc} schema already conforms to {reference_fc}.")
+
+    arcpy.AddMessage("Remove extra fields...")
+
+    schema_conform("bulktrans_districts", r"P:\PROJECTS\Special_Projects\Business_Resiliency_IMT\Data\ArcPRO_Project_ALL_Segmentation\Tools\BulkTrans_segments_HFA_EXAMPLE.shp")
+    schema_conform("subtrans_districts", r"P:\PROJECTS\Special_Projects\Business_Resiliency_IMT\Data\ArcPRO_Project_ALL_Segmentation\Tools\SubTrans_segments_HFA_EXAMPLE.shp")
+
+    arcpy.AddMessage("Export...")
+
+    arcpy.conversion.ExportFeatures("bulktrans_districts", param0)
+    arcpy.conversion.ExportFeatures("subtrans_districts", param1)
+    
+    validate_and_calculate_field(param1, "CIRCUIT_NO", "'ET-'+!CIRCUIT_NO!.split('-')[0]")  # 00158-BT -> ET-00158
 
     return
-
 
 if __name__ == "__main__":
     param0 = arcpy.GetParameterAsText(0)
