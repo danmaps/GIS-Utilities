6391b732fc4411c64159f3c22c1658cbd7d9297e
Danny McVey <dannybmcvey@gmail.com>
2024-10-10 09:10:18 -0700

    nice to haves for this utility

--- src/admin/update_scheduled_task_credentials.ps1
+++ src/admin/update_scheduled_task_credentials.ps1
@@ -2,10 +2,15 @@
 $TaskCredential = Get-Credential
 
 # Let's find those sneaky scheduled tasks that belong to our user
-Get-ScheduledTask |
-    Where-Object { $_.Principal.UserId -eq $TaskCredential.UserName } |
+$tasks = Get-ScheduledTask | Where-Object { $_.Principal.UserId -eq $TaskCredential.UserName }
 
-    # Now, let's give those tasks a fresh new password!
+# Now, let's give those tasks a fresh new password!
+$tasks | ForEach-Object {
     Set-ScheduledTask -User $TaskCredential.UserName -Password $TaskCredential.GetNetworkCredential().Password
+    Write-Output "Updated task: $($_.TaskName)"
+}
 
 # Mission accomplished! Your tasks are now ready to roll with the new password.
+# Keeping the window open to admire our handiwork
+Write-Output "Press any key to exit..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
